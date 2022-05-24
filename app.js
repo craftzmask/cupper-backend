5d5fabedef9ca7ad2c6d5a55b2d83adc531adcca
Khanh Chung <khanh.d.chung@gmail.com>
2022-05-23 19:16:42 -0700

    refactor

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const topRouter = require('./controllers/top')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+
 app.use(middleware.tokenExtractor)
 
 app.use('/api/users', usersRouter)
